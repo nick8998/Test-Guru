3003940e84264d3ff4d20f65bbe2b81db5e2f93d
nick8998 <19nipivanov98@gmail.com>
2020-05-11 12:38:00 +0300

    first

---
+++ config/application.rb
@@ -0,0 +1,16 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module TestGuru
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 6.0
+    config.time_zone = 'Moscow'
+    config.i18n.default_locale = :ru
+  end
+end
