4fcb4044440708198a91d7b95d914ec14dc4c2c1
nick8998 <19nipivanov98@gmail.com>
2020-05-22 12:59:08 +0300

    last

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,5 +1,5 @@
 class Test < ApplicationRecord
   def self.show_tests_by_category(category)
-    Test.joins("INNER JOIN categories ON tests.category_id = categories.id").where(categories: { title: category }).order(id: :desc).pluck(:title)
+    joins("INNER JOIN categories ON tests.category_id = categories.id").where(categories: { title: category }).order(id: :desc).pluck(:title)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   def find_tests_by_lvl(level)
-    Test.joins("INNER JOIN results ON tests.id = results.tests_id").where(tests: { level: level }).where('? = results.users_id', self.select(:id))
+    Test.joins("INNER JOIN results ON tests.id = results.tests_id").where(tests: { level: level }, results: { users_id: id})
   end
 end
