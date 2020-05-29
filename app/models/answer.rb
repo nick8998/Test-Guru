c3939f89b04ad3f5ab3386aa2b1a97d6a760df3b
nick8998 <19nipivanov98@gmail.com>
2020-05-29 13:28:10 +0300

    last

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -7,7 +7,6 @@ class Answer < ApplicationRecord
   validate :validate_new_answer
 
   def validate_new_answer
-    question = Question.where(id: self.question_id).take
     if question.answers.count >= 4
       errors.add(:wording, "can't be created")
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_26_131441) do
+ActiveRecord::Schema.define(version: 2020_05_29_101958) do
 
   create_table "answers", force: :cascade do |t|
     t.string "wording", null: false
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 2020_05_26_131441) do
     t.integer "author_id"
     t.index ["author_id"], name: "index_tests_on_author_id"
     t.index ["category_id"], name: "index_tests_on_category_id"
+    t.index ["title", "level"], name: "index_tests_on_title_and_level", unique: true
   end
 
   create_table "users", force: :cascade do |t|
