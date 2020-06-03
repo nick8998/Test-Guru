baf806a24980076d0702071d5ab743de22623bc6
nick8998 <19nipivanov98@gmail.com>
2020-06-03 17:23:55 +0300

    answer_1

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,15 @@
 class User < ApplicationRecord
-  has_many :results, dependent: :destroy
-  has_many :tests, through: :results, dependent: :destroy
+  has_many :test_passages, dependent: :destroy
+  has_many :tests, through: :test_passages, dependent: :destroy
   has_many :authored_tests, foreign_key: "author_id", class_name: "Test", dependent: :destroy
 
   validates :mail, presence: true
 
   def find_tests_by_lvl(level)
     tests.where(level: level)
+  end
+
+  def test_passage(test)
+    test_passages.order(id: :desc).find_by(test_id: test.id)
   end
 end
